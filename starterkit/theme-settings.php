a125ac612c6d56e43a0dac72044dae1caffeb724
himerus <jake.strawn@gmail.com>
2009-10-24 15:35:52 +0000

    merging DRUPAL-6--1 into HEAD

--- starterkit/theme-settings.php
+++ starterkit/theme-settings.php
@@ -3,6 +3,7 @@
 
 // Include the definition of zen_settings() and zen_theme_get_default_settings().
 include_once './' . drupal_get_path('theme', 'omega') . '/theme-settings.php';
+
 /**
  * Implementation of THEMEHOOK_settings() function.
  *
@@ -11,14 +12,16 @@ include_once './' . drupal_get_path('theme', 'omega') . '/theme-settings.php';
  * @return
  *   A form array.
  */
-function polished_settings($saved_settings) {
+function omega_starterkit_settings($saved_settings) {
   // Get the default values from the .info file.
-  $defaults = omega_theme_get_default_settings('polished');
+  //krumo($saved_settings);
+  $subtheme_defaults = omega_theme_get_default_settings('omega_starterkit');
+  //krumo($defaults);
   // Merge the saved variables and their default values.
-  $settings = array_merge($defaults, $saved_settings);
+  //$settings = array_merge($defaults, $saved_settings);
   $form = array();
   // Add the base theme's settings.
-  $form += omega_settings($saved_settings, $defaults);
+  $form += omega_settings($saved_settings, $subtheme_defaults);
   // Return the form
   return $form;
 }
