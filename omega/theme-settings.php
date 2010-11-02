223d501130f9723ec5aaa9e9380006cebb013d23
himerus <jake.strawn@gmail.com>
2010-11-02 04:17:46 +0000

    nightly commit.. static regions working double dynamic dynamic regions are working as well (sort of)

--- omega/theme-settings.php
+++ omega/theme-settings.php
@@ -4,6 +4,10 @@
 /**
  * @file
  * Theme settings for the Omega theme.
+ * 
+ * @see admin-theme-settings.inc
+ * @see default-theme-settings.inc
+ * @see grid-theme-settings.inc
  */
 
 /**
@@ -15,10 +19,12 @@
  *   A keyed array containing the current state of the form.
  */
 function omega_form_system_theme_settings_alter(&$form, &$form_state) {
+	
 	// provide a warning if the Delta module is not installed
 	if (!module_exists('delta')) {
-		drupal_set_message('<p>Without the <a href="http://himer.us/delta-module"><strong>Delta</strong></a> module, you will only be able to configure grid settings for your theme via the standard form.</p><p>The <a href="http://himer.us/delta-module"><strong>Delta</strong></a> module enables both contextual theme settings and an advanced UI for configuring your <a href="http://himer.us/omega-theme"><strong>Omega</strong></a> subtheme.</p>', 'warning');
+		drupal_set_message('<p>Without the <a href="http://himer.us/delta-module"><strong>Delta</strong></a> module, you will only be able to configure grid settings for your theme via the standard form.</p><p>The <a href="http://himer.us/delta-module"><strong>Delta</strong></a> module enables both contextual theme settings and an advanced UI for configuring your <a href="http://himer.us/omega-theme"><strong>Omega</strong></a> subtheme.</p>', 'error');
 	}
+	
 	// include general theme functions required both in template.php AND theme-settings.php
   require_once(drupal_get_path('theme', 'omega') . '/inc/theme-functions.inc');
   
@@ -29,13 +35,6 @@ function omega_form_system_theme_settings_alter(&$form, &$form_state) {
     )
   );
   
- 
-  
-  //drupal_add_library('system', 'ui.dialog');
-  //drupal_add_library('system', 'ui.draggable');
-  //drupal_add_library('system', 'ui.droppable');
-  
-  
   // Add javascript to show/hide optional settings
   drupal_add_js(drupal_get_path('theme', 'omega') . '/js/omega_admin.js', 
     array(
@@ -45,7 +44,6 @@ function omega_form_system_theme_settings_alter(&$form, &$form_state) {
     )
   );
 
-  
   // include general theme settings
   require_once(drupal_get_path('theme', 'omega') . '/inc/default-theme-settings.inc');
   
@@ -54,5 +52,4 @@ function omega_form_system_theme_settings_alter(&$form, &$form_state) {
   
   // include administrative functions for theme settings
   require_once(drupal_get_path('theme', 'omega') . '/inc/admin-theme-settings.inc');
-  //krumo($form);
 }
