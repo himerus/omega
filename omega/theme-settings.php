adfca0f6abc3907761021c98b8bbddf8256a86c7
himerus <jake.strawn@gmail.com>
2010-11-13 20:18:37 +0000

    turning the omega_admin.js functionality into a Drupal Behavior to work on ajax loaded versions of the theme settings form in the Delta module

--- omega/theme-settings.php
+++ omega/theme-settings.php
@@ -19,7 +19,6 @@
  *   A keyed array containing the current state of the form.
  */
 function omega_form_system_theme_settings_alter(&$form, &$form_state) {
-	
 	// provide a warning if the Delta module is not installed
 	if (!module_exists('omega_ui')) {
 		drupal_set_message('<p>Without the <a href="http://himer.us/omega-ui"><strong>Omega UI</strong></a> module, you will only be able to configure grid settings for your theme via the standard form.</p><p>The <a href="http://himer.us/omega-ui"><strong>Omega UI</strong></a> module enables both contextual theme settings and an advanced UI for configuring your <a href="http://himer.us/omega-theme"><strong>Omega</strong></a> subtheme.</p>', 'warning');
