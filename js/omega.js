95504c2053f26e81574b72813840bdf22041ac72
himerus <jake.strawn@gmail.com>
2011-01-06 00:43:16 +0000

    fixing messed up JS files

--- js/omega.js
+++ js/omega.js
@@ -1,23 +1,23 @@
 // $Id$
 (function ($) {
   function populateElement(selector, defvalue) {
-      if (omega.trim(omega(selector).val()) == "") {
-          omega(selector).val(defvalue);
+      if (omega.trim($(selector).val()) == "") {
+          $(selector).val(defvalue);
       }
-      omega(selector).focus(function() {
-          if(omega(selector).val() == defvalue) {
-              omega(selector).val("");
+      $(selector).focus(function() {
+          if($(selector).val() == defvalue) {
+              $(selector).val("");
           }
       });
-      omega(selector).blur(function() {
-          if(omega.trim(omega(selector).val()) == "") {
-              omega(selector).val(defvalue);
+      $(selector).blur(function() {
+          if(omega.trim($(selector).val()) == "") {
+              $(selector).val(defvalue);
           }
       });
    }
   
   omega = jQuery.noConflict();
-  omega(document).ready(function(){
+  $(document).ready(function(){
     // give the search box some fancy stuff
     populateElement('#search-box input.form-text, #search-block-form input.form-text', Drupal.t(Drupal.settings.default_search_text));
     populateElement('#search-region input.form-text', Drupal.t(Drupal.settings.default_search_text));
