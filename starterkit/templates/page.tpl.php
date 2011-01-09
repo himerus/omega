67b20cd466c23a484afd58fc70ecc1643d8913af
himerus <jake.strawn@gmail.com>
2011-01-09 16:47:09 +0000

    #1020478 - Render action links; Also moving  inside zones-content wrapper

--- starterkit/templates/page.tpl.php
+++ starterkit/templates/page.tpl.php
@@ -5,21 +5,32 @@
  * @file 
  * Theme implementation to display a single Drupal page.
  */
+
 ?>
 <div id="page" class="clearfix">
   <?php if (isset($zones_above)): ?>
   <div id="zones-above" class="clearfix"><?php print $zones_above; ?></div>
   <?php endif; ?>
+  <div id="zones-content" class="clearfix">
+    <?php if ($action_links): ?>
+      <div id="actions-container" class="container-<?php print $default_container_width; ?> clearfix">
+        <div class="grid-<?php print $default_container_width; ?>">
+          <ul class="action-links">
+            <?php print render($action_links); ?>
+          </ul>
+        </div>
+      </div>
+    <?php endif; ?>
+    <?php if (isset($messages)): ?>
+    <div id="message-container" class="container-<?php print $default_container_width; ?> clearfix">
+      <div class="grid-<?php print $default_container_width; ?>">
+        <?php print $messages; ?>
+      </div>
+    </div><!-- /.container-xx -->
+    <?php endif; ?>
   
-  <?php if (isset($messages)): ?>
-  <div id="message-container" class="container-<?php print $default_container_width; ?> clearfix">
-    <div class="grid-<?php print $default_container_width; ?>">
-      <?php print $messages; ?>
-    </div>
-  </div><!-- /.container-xx -->
-  <?php endif; ?>
-
-  <div id="zones-content" class="clearfix"><?php print $content_zone; ?></div>
+    <?php print $content_zone; ?>
+  </div>
   
   <?php if (isset($zones_below)): ?>
   <div id="zones-below" class="clearfix"><?php print $zones_below; ?></div>
