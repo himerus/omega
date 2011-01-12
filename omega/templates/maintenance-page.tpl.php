dc1dbc2ceb809ad0b003bcd07529903c147ddb54
himerus <jake.strawn@gmail.com>
2011-01-12 03:45:36 +0000

    merging omega-DRUPAL-7--2 back to HEAD

---
+++ omega/templates/maintenance-page.tpl.php
@@ -0,0 +1,57 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page while offline.
+ *
+ * All the available variables are mirrored in html.tpl.php and page.tpl.php.
+ * Some may be blank but they are provided for consistency.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_maintenance_page()
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>">
+  <div id="page" class="clearfix">
+    <?php if (isset($zones_above)): ?>
+    <div id="zones-above" class="clearfix"><?php print $zones_above; ?></div>
+    <?php endif; ?>
+    <div id="zones-content" class="clearfix">
+      <?php if (isset($action_links)): ?>
+        <div id="actions-container" class="container-<?php print $default_container_width; ?> clearfix">
+          <div class="grid-<?php print $default_container_width; ?>">
+            <ul class="action-links">
+              <?php print render($action_links); ?>
+            </ul>
+          </div>
+        </div>
+      <?php endif; ?>
+      <?php if (isset($messages)): ?>
+      <div id="message-container" class="container-<?php print $default_container_width; ?> clearfix">
+        <div class="grid-<?php print $default_container_width; ?>">
+          <?php print $messages; ?>
+        </div>
+      </div><!-- /.container-xx -->
+      <?php endif; ?>
+    
+      <?php print $content_zone; ?>
+    </div>
+    
+    <?php if (isset($zones_below)): ?>
+    <div id="zones-below" class="clearfix"><?php print $zones_below; ?></div>
+    <?php endif; ?>
+  </div><!-- /#page -->
+
+</body>
+</html>
--- starterkit/theme-settings.php
+++ starterkit/theme-settings.php
@@ -1,27 +1,19 @@
 <?php
 // $Id$
 
-// Include the definition of zen_settings() and zen_theme_get_default_settings().
-include_once './' . drupal_get_path('theme', 'omega') . '/theme-settings.php';
+/**
+ * @file
+ * Theme settings for the Omega theme.
+ */
 
 /**
- * Implementation of THEMEHOOK_settings() function.
+ * Implements hook_form_system_theme_settings_alter().
  *
- * @param $saved_settings
- *   An array of saved settings for this theme.
- * @return
- *   A form array.
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
  */
-function omega_starterkit_settings($saved_settings) {
-  // Get the default values from the .info file.
-  //krumo($saved_settings);
-  $subtheme_defaults = omega_theme_get_default_settings('omega_starterkit');
-  //krumo($defaults);
-  // Merge the saved variables and their default values.
-  //$settings = array_merge($defaults, $saved_settings);
-  $form = array();
-  // Add the base theme's settings.
-  $form += omega_settings($saved_settings, $subtheme_defaults);
-  // Return the form
-  return $form;
+function omega_starterkit_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Include any changes to the theme settings here. 
 }
