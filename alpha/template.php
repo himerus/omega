9134512af1aae235d8eba335b683fc29f5c258ff
Sebastian Siemssen <sebastian@dr-siemssen.de>
2011-04-18 23:46:08 +0200

    Added support for disabling / excluding core, contrib module and theme stylesheets[] definitions via the Omega / Alpha theme settings

--- alpha/template.php
+++ alpha/template.php
@@ -71,54 +71,17 @@ function alpha_theme_registry_alter(&$registry) {
 
 /**
  * Implements hook_css_alter().
- * @TODO: Once http://drupal.org/node/901062 is resolved, determine whether
- * this can be implemented in the .info file instead.
- *
- * Omitted:
- * - color.css
- * - contextual.css
- * - dashboard.css
- * - field_ui.css
- * - image.css
- * - locale.css
- * - shortcut.css
- * - simpletest.css
- * - toolbar.css
  */
 function alpha_css_alter(&$css) {
-  //krumo($css);
-  $exclude = array(
-    //'misc/vertical-tabs.css' => FALSE,
-    'modules/aggregator/aggregator.css' => FALSE,
-    'modules/block/block.css' => FALSE,
-    'modules/book/book.css' => FALSE,
-    'modules/comment/comment.css' => FALSE,
-    'modules/dblog/dblog.css' => FALSE,
-    'modules/file/file.css' => FALSE,
-    'modules/filter/filter.css' => FALSE,
-    'modules/forum/forum.css' => FALSE,
-    'modules/help/help.css' => FALSE,
-    'modules/menu/menu.css' => FALSE,
-    'modules/node/node.css' => FALSE,
-    'modules/openid/openid.css' => FALSE,
-    'modules/poll/poll.css' => FALSE,
-    'modules/profile/profile.css' => FALSE,
-    'modules/search/search.css' => FALSE,
-    'modules/statistics/statistics.css' => FALSE,
-    'modules/syslog/syslog.css' => FALSE,
-    'modules/system/admin.css' => FALSE,
-    'modules/system/maintenance.css' => FALSE,
-    'modules/system/system.css' => FALSE,
-    'modules/system/system.admin.css' => FALSE,
-    //'modules/system/system.base.css' => FALSE,
-    'modules/system/system.maintenance.css' => FALSE,
-    'modules/system/system.menus.css' => FALSE,
-    //'modules/system/system.messages.css' => FALSE,
-    'modules/system/system.theme.css' => FALSE,
-    'modules/taxonomy/taxonomy.css' => FALSE,
-    'modules/tracker/tracker.css' => FALSE,
-    'modules/update/update.css' => FALSE,
-    'modules/user/user.css' => FALSE,
-  );
-  $css = array_diff_key($css, $exclude);
+  global $theme_key;
+  
+  $settings = alpha_settings($theme_key);
+  
+  if (!empty($settings['exclude'])) {
+    foreach ($settings['exclude'] as $file => $exclude) {
+      if ($exclude) {
+        unset($css[$file]);
+      }
+    }
+  }
 }
