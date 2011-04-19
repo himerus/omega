ae6a6e854ec57ad162a139fcd5ce4469382d673d
Sebastian Siemssen <sebastian@dr-siemssen.de>
2011-04-20 01:36:36 +0200

    Changed the section / zone configuration in the theme-settings UI. Zones can no longer be disabled, instead they can be moved out of regions. Zones dont have their own vertical tab anymore, instead they are being configured 'inline' inside of the section configuration.

--- alpha/theme-settings.php
+++ alpha/theme-settings.php
@@ -14,7 +14,6 @@ function alpha_form_system_theme_settings_alter(&$form, &$form_state) {
   $form_state['#regions'] = alpha_regions($theme);
   $form_state['#containers'] = alpha_container_options($theme, alpha_theme_get_setting('alpha_grid', $theme, 'default'));
      
-  require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-zones.inc';
   require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-general.inc';
   require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-sections.inc';
   require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-regions.inc';
@@ -23,7 +22,6 @@ function alpha_form_system_theme_settings_alter(&$form, &$form_state) {
     drupal_set_message(t('You have the !delta module installed, enabling advanced contextual theme settings. The settings provided on this page serve as the default theme settings when creating a new !delta theme settings template, yet can be overwritten by !delta based on the settings of your !delta templates and overrides.', array('!delta' => '<a href="http://drupal.org/project/delta"><strong>Delta</strong></a>')));
   }
   
-  alpha_theme_settings_zones($form, $form_state);   
   alpha_theme_settings_general($form, $form_state);
   alpha_theme_settings_sections($form, $form_state);
   alpha_theme_settings_regions($form, $form_state);
