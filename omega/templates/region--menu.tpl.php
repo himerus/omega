b2115c76452e8087cc113750830f17781000da67
@himerus <jake.strawn@gmail.com>
2011-04-19 21:49:01 -0400

    updates to default CSS and templates

--- omega/templates/region--menu.tpl.php
+++ omega/templates/region--menu.tpl.php
@@ -1,9 +1,9 @@
 <div<?php print $attributes; ?>>
 <?php if ($main_menu || $secondary_menu): ?>
-  <div id="navigation">
-    <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-    <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-  </div>
+  <nav class="navigation">
+    <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+    <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+  </nav>
 <?php endif; ?>
 <?php print $content; ?>
 </div>
