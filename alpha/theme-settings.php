d903fe51abf4a03f8292cb2fa3956b67b4ec3c8c
Sebastian Siemssen <sebastian@dr-siemssen.de>
2011-04-21 00:17:17 +0200

    Removed the comment on alpha_theme_get_setting that said that we needed to change the function in order to make it work with Delta... Because THATS NOT TRUE ANYMORE - Delta works independently now.

--- alpha/theme-settings.php
+++ alpha/theme-settings.php
@@ -17,11 +17,7 @@ function alpha_form_system_theme_settings_alter(&$form, &$form_state) {
   require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-general.inc';
   require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-zones.inc';
   require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'alpha') . '/includes/theme-settings-regions.inc';
-  
-  if (module_exists('delta') && arg(2) != 'delta') {
-    drupal_set_message(t('You have the !delta module installed, enabling advanced contextual theme settings. The settings provided on this page serve as the default theme settings when creating a new !delta theme settings template, yet can be overwritten by !delta based on the settings of your !delta templates and overrides.', array('!delta' => '<a href="http://drupal.org/project/delta"><strong>Delta</strong></a>')));
-  }
-  
+
   alpha_theme_settings_general($form, $form_state);
   alpha_theme_settings_zones($form, $form_state);
   alpha_theme_settings_regions($form, $form_state);
