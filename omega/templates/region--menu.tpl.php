be6e956c82a55c136c3c4556073ffddcb1a88c53
@himerus <jake.strawn@gmail.com>
2011-04-19 21:20:17 -0400

    adding back in most HTML5 and RDFa items back into Omega

---
+++ omega/templates/region--menu.tpl.php
@@ -0,0 +1,9 @@
+<div<?php print $attributes; ?>>
+<?php if ($main_menu || $secondary_menu): ?>
+  <div id="navigation">
+    <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+    <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+  </div>
+<?php endif; ?>
+<?php print $content; ?>
+</div>
--- omega/templates/zone--content.tpl.php
+++ omega/templates/zone--content.tpl.php
@@ -1,11 +1,5 @@
 <?php if ($wrapper) : ?><div<?php print $wrapper_attributes; ?>><?php endif; ?>  
   <div<?php print $attributes; ?>>
-    <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation" class="grid-<?php print $columns; ?>">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div>
-    <?php endif; ?>
     
     <?php if ($breadcrumb): ?>
       <div id="breadcrumb" class="grid-<?php print $columns; ?>"><?php print $breadcrumb; ?></div>
