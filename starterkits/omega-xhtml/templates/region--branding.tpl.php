b437d7f2773ad2f3b946a9e17e3f0c09f32b4f1e
Sebastian Siemssen <sebastian@dr-siemssen.de>
2011-04-23 17:24:40 +0200

    Changed the branding region in the xhtml starterkit according to the latest changes in the Omega base theme.

--- starterkits/omega-xhtml/templates/region--branding.tpl.php
+++ starterkits/omega-xhtml/templates/region--branding.tpl.php
@@ -1,20 +1,25 @@
 <div<?php print $attributes; ?>>
   <div class="branding-data clearfix">
-    <?php if(isset($linked_logo_img)): ?>
+    <?php if (isset($linked_logo_img)): ?>
     <div class="logo-img">
       <?php print $linked_logo_img; ?>
     </div>
     <?php endif; ?>
-    <div class="site-name-slogan">
+    <?php if ($site_name || $site_slogan): ?>
+    <?php $class = $site_name_hidden && $site_slogan_hidden ? ' element-invisible' : ''; ?>
+    <div class="site-name-slogan<?php print $class; ?>">
+      <?php $class = $site_name_hidden && !$site_slogan_hidden ? ' element-invisible' : ''; ?>
       <?php if ($is_front): ?>
-      <h1 class="site-title"><?php print $linked_site_name; ?></h1>
+      <h1 class="site-title<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
       <?php else: ?>
-      <h2 class="site-title"><?php print $linked_site_name; ?></h2>
+      <h2 class="site-title<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
       <?php endif; ?>
-      <?php if(isset($site_slogan)): ?>
-      <h6 class="site-slogan"><?php print $site_slogan; ?></h6>
+      <?php print ($site_slogan_hidden && !$site_name_hidden ? ' element-invisible' : ''); ?>
+      <?php if ($site_slogan): ?>
+      <h6 class="site-slogan<?php print $class; ?>"><?php print $site_slogan; ?></h6>
       <?php endif; ?>
     </div>
+    <?php endif; ?>
   </div>
   <?php print $content; ?>
 </div>
