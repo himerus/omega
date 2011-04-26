f0f904db67e7b182d5969cea2a21dd3fa115560f
Sebastian Siemssen <sebastian@dr-siemssen.de>
2011-04-26 13:39:49 +0200

    Trouble during commiting... should be fixed now.

--- omega/theme-settings.php
+++ omega/theme-settings.php
@@ -2,9 +2,10 @@
 
 function omega_form_system_theme_settings_alter(&$form, &$form_state) {
   $settings = $form_state['alpha_settings'];
+  
   $form['alpha_settings']['styles']['omega_formalize'] = array(
     '#type' => 'checkbox',
-    '#default_value' => isset($settings['omega']['formalize']) ? $settings['omega']['formalize'] : '',
+    '#default_value' => $settings['omega']['formalize'],
     '#title' => t('Enable Formalize by <a href="http://formalize.me/" title="Formalize">Nathan Smith</a>'),
     '#description' => t('Formalize is a framework by Nathan Smith for neat looking cross-browser forms with extended functionality.'),
     '#weight' => -10,
