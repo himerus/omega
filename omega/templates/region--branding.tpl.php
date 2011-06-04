4c475b0dc5d7c0628ae94d6ef38ca92dc5b87a08
Sebastian Siemssen <sebastian@dr-siemssen.de>
2011-06-04 14:17:50 +0200

    CSS names were wrongly targeting site-title where it should be site-name.

--- omega/templates/region--branding.tpl.php
+++ omega/templates/region--branding.tpl.php
@@ -12,9 +12,9 @@
       <hgroup class="site-name-slogan<?php print $class; ?>">
         <?php $class = $site_name_hidden && !$site_slogan_hidden ? ' element-invisible' : ''; ?>
         <?php if ($is_front): ?>
-        <h1 class="site-title<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
+        <h1 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
         <?php else: ?>
-        <h2 class="site-title<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
+        <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
         <?php endif; ?>
         <?php print ($site_slogan_hidden && !$site_name_hidden ? ' element-invisible' : ''); ?>
         <?php if ($site_slogan): ?>
