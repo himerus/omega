283835148313fabf73100c86b44ea6d98a882972
fubhy <sebastian@dr-siemssen.de>
2011-07-24 16:46:38 +0200

    Issue 1228016: Major code review and performance enhancements. Needs testing.

--- omega/js/omega-equalheights.js
+++ omega/js/omega-equalheights.js
@@ -6,38 +6,31 @@
   /**
    * @todo
    */
-  var omegaEqualHeights = function (elements) {  
-    elements.unbind('resize.omegaequalheights');
-    
-    $(elements).css('height', '').css('min-height', '');
-    
-    if (!Drupal.behaviors.hasOwnProperty('omegaMediaQueries') || Drupal.omega.getCurrentLayout() != 'mobile') {
-      var tallest = 0;
-      
-      elements.each(function () {    
-        if ($(this).height() > tallest) {
-          tallest = $(this).height();
-        }
-      }).each(function() {
-        if ($(this).height() < tallest) {
-          $(this).css('height', tallest).css('min-height', tallest);
-        }
-      });
-    }
-    
-    elements.bind('resize.omegaequalheights', function () {
-      omegaEqualHeights(elements);
-    });
-  }
-  
-  /**
-   * @todo
-   */
   Drupal.behaviors.omegaEqualHeights = {
     attach: function (context) {
       $('body', context).once('omega-equalheights', function () {
-        $($('.equal-height-container').get().reverse()).each(function () {
-          omegaEqualHeights($(this).children('.equal-height-element'));
+        $(window).bind('resize.omegaequalheights', function () {
+          $($('.equal-height-container').get().reverse()).each(function () {
+            var elements = $(this).children('.equal-height-element');
+            
+            $(elements).css('height', '');
+            
+            if (!Drupal.behaviors.hasOwnProperty('omegaMediaQueries') || Drupal.omega.getCurrentLayout() != 'mobile') {
+              var tallest = 0;
+
+              elements.each(function () {    
+                if ($(this).height() > tallest) {
+                  tallest = $(this).height();
+                }
+              }).each(function() {
+                if ($(this).height() < tallest) {
+                  $(this).css('height', tallest);
+                }
+              });
+            }
+          });
+        }).load(function () {
+          $(this).trigger('resize.omegaequalheights');
         });
       });
     }
