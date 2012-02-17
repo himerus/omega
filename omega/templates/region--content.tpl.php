7b85831fe942cd1b93c51c142743b2a7c0384813
Hydra <Hydra@647364.no-reply.drupal.org>
2012-02-17 16:53:38 -0500

    fix for #1332732

--- omega/templates/region--content.tpl.php
+++ omega/templates/region--content.tpl.php
@@ -8,7 +8,7 @@
     <?php print render($title_suffix); ?>
     <?php if ($title_hidden): ?></div><?php endif; ?>
     <?php endif; ?>
-    <?php if ($tabs): ?><div class="tabs clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+    <?php if ($tabs && !empty($tabs['#primary'])): ?><div class="tabs clearfix"><?php print render($tabs); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print $content; ?>
     <?php if ($feed_icons): ?><div class="feed-icon clearfix"><?php print $feed_icons; ?></div><?php endif; ?>
