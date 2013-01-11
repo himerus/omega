3c0fb959600883f484d86cb4c8379b4d19c2ec54
Sebastian Siemssen <fubhy@fubhy.com>
2013-01-11 14:31:32 +0100

    Massive cleanup and improvement of drush integration and the starterkit / subtheme generator in particular.

--- starterkits/default/template.php
+++ starterkits/default/template.php
@@ -3,5 +3,5 @@
 /**
  * @file
  * Template overrides as well as (pre-)process and alter hooks for the
- * Default Omega Starterkit theme.
+ * {{ THEMENAME }} theme.
  */
--- starterkits/default/theme-settings.php
+++ starterkits/default/theme-settings.php
@@ -10,7 +10,7 @@ require_once dirname(__FILE__) . '/template.php';
 /**
  * Implements hook_form_FORM_alter().
  */
-function omega_starterkit_form_system_theme_settings_alter(&$form, $form_state) {
+function {{ THEME }}_form_system_theme_settings_alter(&$form, $form_state) {
   // You can use this hook to append your own theme settings to the theme
   // settings form for your subtheme. However, you should also take a look at
   // the 'extensions' concept in the Omega base theme. We highly suggest that you
