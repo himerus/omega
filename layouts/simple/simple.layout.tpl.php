c75aad0541e3c5fa11012b59c3431d00b1698997
Matt Smith <matt@takerootdesign.co.uk>
2013-01-22 21:58:09 +0000

    Initial commit of the 'simple' omega layout.

---
+++ layouts/simple/simple.layout.tpl.php
@@ -0,0 +1,62 @@
+<div class="page">
+
+  <header class="header" role="banner">
+
+    <div class="branding">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <?php endif; ?>
+
+        <?php if ($site_name || $site_slogan): ?>
+          <hgroup>
+            <?php if ($site_name): ?>
+              <h1 class="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
+
+            <?php if ($site_slogan): ?>
+              <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+            <?php endif; ?>
+          </hgroup>
+        <?php endif; ?>
+    </div>
+
+    <?php print render($page['header']); ?>
+
+    <?php print render($page['navigation']); ?>
+
+  </header>
+
+  <div class="main">
+
+    <div class="content" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+
+    <?php print render($page['sidebar_first']); ?>
+
+    <?php print render($page['sidebar_second']); ?>
+
+  </div>
+
+  <footer class="footer" role="contentinfo">
+    <?php print render($page['footer']); ?>
+  </footer>
+
+</div>
