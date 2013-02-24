16bcde679870243b621fdbc57b6e0af1e568f509
Matt Smith <matt@takerootdesign.co.uk>
2013-02-24 12:39:12 +0000

    Added l- prefix to omega page.tpl.php

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -61,67 +61,63 @@
  * - $page['sidebar_first']: Items for the first sidebar.
  * - $page['sidebar_second']: Items for the second sidebar.
  * - $page['footer']: Items for the footer region.
- * - $page['bottom']: Items to appear at the bottom of the page below the footer.
  *
  * @see template_preprocess()
  * @see template_preprocess_page()
  * @see template_process()
  */
 ?>
-
-<header id="header" role="banner">
-  <?php if ($logo): ?>
-    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-  <?php endif; ?>
-
-  <?php if ($site_name || $site_slogan): ?>
-    <hgroup id="name-and-slogan">
-      <?php if ($site_name): ?>
-        <h1 id="site-name">
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-        </h1>
+<div class="l-page">
+  <header class="l-header" role="banner">
+    <div class="l-branding">
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
       <?php endif; ?>
 
-      <?php if ($site_slogan): ?>
-        <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+      <?php if ($site_name || $site_slogan): ?>
+        <hgroup>
+          <?php if ($site_name): ?>
+            <h1 class="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+          <?php endif; ?>
+        </hgroup>
       <?php endif; ?>
-    </hgroup>
-  <?php endif; ?>
+    </div>
 
-  <?php print render($page['header']); ?>
-</header>
+    <?php print render($page['header']); ?>
+    <?php print render($page['navigation']); ?>
+  </header>
 
-<div id="main">
-  <div id="main-content" role="main">
-    <?php print render($page['highlighted']); ?>
-    <a id="main-content-anchor"></a>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1 class="title" id="page-title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php print $messages; ?>
-    <?php print render($tabs); ?>
-    <?php print render($page['help']); ?>
-    <?php if ($action_links): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-    <?php print render($page['content']); ?>
+  <div class="l-main">
+    <div class="l-content" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+
+    <?php print render($page['sidebar_first']); ?>
+    <?php print render($page['sidebar_second']); ?>
   </div>
 
-  <?php if (render($page['navigation'])): ?>
-    <div id="navigation">
-      <?php print render($page['navigation']); ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if (render($page['sidebar_first']) || render($page['sidebar_second'])): ?>
-    <aside class="sidebars">
-      <?php print render($page['sidebar_first']); ?>
-      <?php print render($page['sidebar_second']); ?>
-    </aside>
-  <?php endif; ?>
+  <footer class="l-footer" role="contentinfo">
+    <?php print render($page['footer']); ?>
+  </footer>
 </div>
-
-<?php print render($page['footer']); ?>
-<?php print render($page['bottom']); ?>
