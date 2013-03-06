7c9431f52a25cde49aecfc10bb5e1411fef87d7c
Sebastian Siemssen <fubhy@fubhy.com>
2013-03-06 11:48:58 +0100

    Making the cache_clear_all() calls more specific.

--- theme-settings.php
+++ theme-settings.php
@@ -172,7 +172,7 @@ function omega_form_system_theme_settings_alter(&$form, &$form_state, $form_id =
 function omega_theme_settings_form_submit($form, &$form_state) {
   // Clear the theme cache.
   $theme = $form_state['build_info']['args'][0];
-  cache_clear_all('omega:' . $theme, 'cache', TRUE);
+  cache_clear_all('omega:' . $theme . ':', 'cache', TRUE);
 
   // We also need to clear the static right away.
   drupal_static_reset('omega_extensions');
