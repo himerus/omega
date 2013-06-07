9c042f52a65686bd2c537b54652fb28ae62900ff
Sebastian Siemssen <fubhy@fubhy.com>
2013-06-07 17:35:05 +0200

    Formatting ohm hero layout template.

--- layouts/ohm-hero/ohm-hero-layout.tpl.php
+++ layouts/ohm-hero/ohm-hero-layout.tpl.php
@@ -5,15 +5,11 @@
         <?php if ($logo): ?>
           <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
         <?php endif; ?>
-
-        <?php if ($site_name || $site_slogan): ?>
         <?php if ($site_name): ?>
-              <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          <?php endif; ?>
-
+          <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+        <?php endif; ?>
         <?php if ($site_slogan): ?>
-              <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
-          <?php endif; ?>
+          <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
       </div>
       <?php print render($page['navigation']); ?>
@@ -22,6 +18,7 @@
   </header>
 
   <?php print render($page['hero']); ?>
+
   <div class="l-highlighted-wrapper">
     <?php print render($page['highlighted']); ?>
   </div>
