7c9b96ddd549a92176069138d9967e4e311a81c5
Sebastian Siemssen <fubhy@fubhy.com>
2013-07-18 12:47:20 +0200

    Adding Gruntfile and package.json to starterkits. This means we now support GruntJS. Built-in features: UglifyJS, Compass, LiveReload, JSLint.

---
+++ starterkits/extended/Gruntfile.js
@@ -0,0 +1,93 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        livereload: true
+      },
+      sass: {
+        files: ['sass/{,**/}*.{scss,sass}'],
+        tasks: ['compass:dev']
+      },
+      images: {
+        files: ['images/**']
+      },
+      css: {
+        files: ['stylesheets/{,**/}*.css']
+      },
+      js: {
+        files: ['js/{,**/}*.js', '!js/{,**/}*.js'],
+        tasks: ['jshint', 'uglify:dev']
+      }
+    },
+
+    compass: {
+      options: {
+        config: 'config.rb',
+        bundleExec: true
+      },
+      dev: {
+        options: {
+          environment: 'development',
+          force: true
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production',
+          force: true
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['js/{,**/}*.js', '!js/{,**/}*.min.js']
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          mangle: false,
+          compress: false,
+          beautify: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'js',
+          src: ['**/*.js', '!**/*.min.js'],
+          dest: 'js',
+          ext: '.min.js'
+        }]
+      },
+      dist: {
+        options: {
+          mangle: true,
+          compress: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'js',
+          src: ['**/*.js', '!**/*.min.js'],
+          dest: 'js',
+          ext: '.min.js'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', [
+    'uglify:dist',
+    'compass:dist',
+    'jshint'
+  ]);
+};
