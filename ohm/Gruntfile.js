0df9025af4b33cb1ce013fa568adacf24d1a1ae3
Sebastian Siemssen <fubhy@fubhy.com>
2013-07-19 14:40:08 +0200

    Re-adding bower.json to Ohm for people who actually want to use Ohm directly.

--- ohm/Gruntfile.js
+++ ohm/Gruntfile.js
@@ -47,14 +47,60 @@ module.exports = function (grunt) {
         jshintrc: '.jshintrc'
       },
       all: ['js/{,**/}*.js', '!js/{,**/}*.min.js']
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          mangle: false,
+          compress: false,
+          beautify: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'js',
+          src: ['**/*.js', '!**/*.min.js'],
+          dest: 'js',
+          ext: '.min.js'
+        }]
+      },
+      dist: {
+        options: {
+          mangle: true,
+          compress: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'js',
+          src: ['**/*.js', '!**/*.min.js'],
+          dest: 'js',
+          ext: '.min.js'
+        }]
+      },
+      components: {
+        options: {
+          mangle: false,
+          compress: false
+        },
+        files: {
+          'components/matchmedia/matchMedia.min.js': [
+            'components/matchmedia/matchMedia.js',
+            'components/matchmedia/matchMedia.addListener.js'
+          ],
+          'components/selectivizr/selectivizr.min.js': ['components/selectivizr/selectivizr.js'],
+          'components/css3mediaqueries/css3-mediaqueries.min.js': ['components/css3mediaqueries/css3-mediaqueries.js']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('build', [
+    'uglify:dist',
     'compass:dist',
     'jshint'
   ]);
