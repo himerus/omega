ba91845a7a8ea3f9c1773e6537c78ee440f6a5d1
Sebastian Siemssen <fubhy@fubhy.com>
2013-07-19 14:24:44 +0200

    Cleaning up unused files in Ohm, rebuilding the config.rb file and re-compiling assets.

--- ohm/config.rb
+++ ohm/config.rb
@@ -6,9 +6,6 @@
 ## file for more information.
 ##
 
-# Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-
 # Location of the theme's resources.
 css_dir = "css"
 sass_dir = "sass"
@@ -28,7 +25,7 @@ require 'susy'
 
 # You can select your preferred output style here (:expanded, :nested, :compact
 # or :compressed).
-output_style = (environment == :development) ? :expanded : :expanded
+output_style = (environment == :production) ? :expanded : :expanded
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
@@ -36,7 +33,7 @@ output_style = (environment == :development) ? :expanded : :expanded
 relative_assets = true
 
 # Conditionall enable line comments when in development mode.
-line_comments = (environment == :development) ? true : false
+line_comments = (environment == :production) ? false : true
 
 # Output debugging info in development mode.
-sass_options = (environment == :development) ? {:debug_info => true} : {}
+sass_options = (environment == :production) ? {} : {:debug_info => true}
