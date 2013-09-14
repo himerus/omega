85ae0d0b9c5a8f8e5864cb527e5ec5bd9f59eec7
Sebastian Siemssen <fubhy@fubhy.com>
2013-09-14 15:38:39 +0200

    Further decrease the weight of the warning FAPI element.

--- ohm/theme-settings.php
+++ ohm/theme-settings.php
@@ -16,6 +16,6 @@ function ohm_form_system_theme_settings_alter(&$form, $form_state) {
     '#title' => t('Show a warning when this theme is used'),
     '#description' => t("You can permanently hide this message, but please be aware that Ohm is a demonstration subtheme and will therefore be constantly evolving with latest best practices. Explore, break it and learn but don't use it in production directly, or as a base theme."),
     '#default_value' => omega_theme_get_setting('ohm_enable_warning', TRUE),
-    '#weight' => -10,
+    '#weight' => -20,
   );
 }
