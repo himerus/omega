c42ab76c9939b9ea19de489c49da0abd3b8af79f
Jake Strawn <jake.strawn@gmail.com>
2017-05-06 10:39:50 -0400

    Making component footer handle toolbar adjustments better.

--- omega/js/component--footer.js
+++ omega/js/component--footer.js
@@ -2,6 +2,27 @@
 
     "use strict";
 
+    var omegaAdjustContainers = function (offset) {
+        var toolbarExists = $('#toolbar-administration').length;
+
+        if (!toolbarExists) {
+            return false;
+        }
+
+        // The elements that should obtain "100%" height at minimum.
+        var elements = [
+            '.page--wrapper',
+            '.page--wrapper .page',
+            '.page--wrapper .page .omega-layout-wrapper',
+            '.page--wrapper .page .omega-layout'
+        ];
+
+        var $heightElements = $(elements.join(', '));
+
+        // Apply a calculated value the height of the elements.
+        $heightElements.css('min-height', 'calc(100vh - ' + offset + 'px)');
+    }
+
     /**
      * Function to handle adjusting the padding when the footer grows/shrinks.
      */
@@ -12,6 +33,7 @@
             var $footer = $('.region-group--footer');
             // The layout grouping wrapper around the particular region.
             var $wrapper = $footer.closest('.omega-layout');
+            //var $wrapper = $('body');
 
             $(window).on('resize ready load', function () {
                 // Find the actual height of the footer and its contents.
@@ -22,4 +44,16 @@
         }
     };
 
+    /**
+     * Function to handle adjusting the height of primary container elements for a footer fixed to the bottom of the screen..
+     */
+    Drupal.behaviors.bodyElementAdjust = {
+        attach: function (context, settings) {
+
+            // Handle adjustments to toolbar tray visibility.
+            $(document).on('drupalViewportOffsetChange.toolbar', function(event, offsets){
+                omegaAdjustContainers(offsets.top);
+            });
+        }
+    };
 })(jQuery, Modernizr, Drupal, drupalSettings, window);
