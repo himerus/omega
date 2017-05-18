cbfa174f5991b5238dc4670729492666fa591257
Jake Strawn <jake.strawn@gmail.com>
2017-05-18 12:06:28 -0400

    Disable selector-max-specificity for VERY intentionally specific rules in generated layout SCSS.

--- omega/src/Layout/OmegaLayout.php
+++ omega/src/Layout/OmegaLayout.php
@@ -319,11 +319,14 @@ class OmegaLayout implements OmegaLayoutInterface {
         }
         // check to see if primary region is set
         if ($primary_region && $total_regions <= 3) {
-          $breakpoint_scss .= "\n\n      " . '// A primary region exists for the ' . $gid . ' region group.';
+          $breakpoint_scss .= "\n\n      " . '/* stylelint-disable selector-max-specificity  */';
+          $breakpoint_scss .= "\n      " . '// A primary region exists for the ' . $gid . ' region group.';
           $breakpoint_scss .= "\n      " . '// so we are going to iterate over combinations of available/missing';
           $breakpoint_scss .= "\n      " . '// regions to change the layout for this group based on those scenarios.';
 
           $breakpoint_scss .= "\n\n      " . '// 1 missing region';
+
+
 
           // loop over the regions that are not the primary one again
           $mainRegion = $layout['groups'][$idtrim][$gid]['regions'][$primary_region];
@@ -457,6 +460,7 @@ class OmegaLayout implements OmegaLayoutInterface {
               } // end if ($regionname != $regionname2)
             } // end foreach $otherRegions (2nd loop)
           }  // end foreach $otherRegions (1st loop)
+          $breakpoint_scss .= "\n    /* stylelint-enable selector-max-specificity  */";
         }  // end if($primary_region)
         $breakpoint_scss .= "\n  " . '  }'; // end of region group
       }
